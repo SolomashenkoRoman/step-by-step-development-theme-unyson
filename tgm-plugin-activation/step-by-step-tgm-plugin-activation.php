9eacf4f898787e52390febbc1eae9a933d5e14a2
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-19 22:34:58 +0200

    Установка плагинов с помощью TGM Plugin Activation

--- tgm-plugin-activation/step-by-step-tgm-plugin-activation.php
+++ tgm-plugin-activation/step-by-step-tgm-plugin-activation.php
@@ -66,7 +66,7 @@ function step_by_step_development_theme_unyson_register_required_plugins() {
 			'source'             => get_template_directory() . '/lib/plugins/unyson.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
 			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
