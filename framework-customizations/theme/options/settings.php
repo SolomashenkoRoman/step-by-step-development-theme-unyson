87dfd36bffc7cfbb52461f418ccf67b2f5a5de22
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 12:31:00 +0200

    Options Built-in Option Types Text

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -27,4 +27,15 @@ $options = array(
         'label' => __('Body Color', MY_THEME_TEXTDOMAIN),
         'value' => '#ADFF2F',
     ),
+    'option_regular_text' => array(
+        'type'  => 'text',
+        'value' => 'Default value',
+        'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
+    )
 );
