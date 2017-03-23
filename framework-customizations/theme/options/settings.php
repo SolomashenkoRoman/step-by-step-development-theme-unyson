b9d907db0908e66997a1972666ecf4da85e88f31
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 15:29:32 +0200

    Options Built-in Option Types wp-editor

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -648,5 +648,22 @@ $options = array(
         'label' => __('Label', '{domain}'),
         'desc'  => __('Description', '{domain}'),
         'help'  => __('Help tip', '{domain}'),
+    ),
+    'option_wp_editor' => array(
+        'type'  => 'wp-editor',
+        'value' => 'default value',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        'size' => 'small', // small, large
+        'editor_height' => 400,
+        'wpautop' => true,
+        'editor_type' => false, // tinymce, html
+
+        /**
+         * Also available
+         * https://github.com/WordPress/WordPress/blob/4.4.2/wp-includes/class-wp-editor.php#L80-L94
+         */
     )
 );
