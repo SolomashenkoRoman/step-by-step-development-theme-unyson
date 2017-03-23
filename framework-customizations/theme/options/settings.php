b1ef5b6ddbb2a35e08824a298e27948a7f6cd332
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 14:08:54 +0200

    Options Built-in Option Types background image

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -291,4 +291,34 @@ $options = array(
             ),
         'blank' => true, // (optional) Если true, изображения могут быть отменены
     ),
+    'option_background_image' => array(
+        'type'  => 'background-image',
+        'value' => 'bg-1',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        'choices' => array(
+            'none' => array(
+                'icon' => get_template_directory_uri() . '/images/bg/bg-0.jpg',
+                'css'  => array(
+                    'background-image' => 'none'
+                ),
+            ),
+            'bg-1' => array(
+                'icon'  => get_template_directory_uri() . '/images/bg/bg-1.jpg',
+                'css'  => array(
+                    'background-image'  => 'url("' . get_template_directory_uri() . '/images/bg/bg-1.png' . '")',
+                    'background-repeat' => 'repeat',
+                ),
+            ),
+            'bg-2' => array(
+                'icon' => get_template_directory_uri() . '/images/bg/bg-2.jpg',
+                'css'  => array(
+                    'background-image'  => 'url("' . get_template_directory_uri() . '/images/bg/bg-2.png' . '")',
+                    'background-repeat' => 'repeat-y'
+                ),
+            )
+        )
+    ),
 );
