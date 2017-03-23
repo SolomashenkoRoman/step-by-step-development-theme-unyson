1861df1322721d059f496e99c321237c0e0f68c6
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 15:16:15 +0200

    Options Built-in Option Types addable-popup

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -545,5 +545,45 @@ $options = array(
                 ),
             ),
         ),
-    )
+    ),
+    'option_addable_popup' => array(
+        'type' => 'addable-popup',
+        'value' => array(
+            array(
+                'addable_popup_option_1' => 'value 1',
+                'addable_popup_option_2' => 'value 2',
+            ),
+            // ...
+        ),
+        'label' => __('Addable Popup', '{domain}'),
+        'desc'  => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+        'template' => '{{- addable_popup_option_1 }}',
+        'popup-title' => null,
+        'size' => 'small', // small, medium, large
+        'limit' => 0, // limit the number of popup`s that can be added
+        'add-button-text' => __('Add', '{domain}'),
+        'sortable' => true,
+        'popup-options' => array(
+            'addable_popup_option_1' => array(
+                'label' => __('Text', '{domain}'),
+                'type' => 'text',
+                'value' => 'Demo text value',
+                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
+                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '{domain}')
+                ),
+            ),
+            'addable_popup_option_2' => array(
+                'label' => __('Textarea', '{domain}'),
+                'type' => 'textarea',
+                'value' => 'Demo textarea value',
+                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
+                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '{domain}')
+                ),
+            ),
+        ),
+    ),
 );
