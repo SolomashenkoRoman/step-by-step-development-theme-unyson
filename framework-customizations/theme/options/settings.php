f0c371fcedb1811736b141ce5fbdad55fcd44e62
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 13:28:48 +0200

    Options Built-in Option Types color picker

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -223,4 +223,14 @@ $options = array(
         ),
 
     ),
+    'option_color_picker' => array(
+        'type'  => 'color-picker',
+        'value' => '#FF0000',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        // Палитра цветов
+        'palettes' => array( '#ba4e4e', '#0ce9ed', '#941940' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+    )
 );
