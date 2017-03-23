aa523509256451c68ff6b61a19c609c5aeaecb6e
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 12:33:59 +0200

    Options Built-in Option Types Textarea

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -37,5 +37,16 @@ $options = array(
             'data-foo' => 'bar'
         ),
         'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
+    ),
+    'option_regular_textarea' => array(
+        'type'  => 'textarea',
+        'value' => 'Default value',
+        'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
     )
 );
