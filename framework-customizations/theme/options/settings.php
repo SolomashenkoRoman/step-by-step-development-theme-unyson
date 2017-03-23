fa3e44bb7bfe10fcd268f6963e88807c7e1b8f50
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 15:19:19 +0200

    Options Built-in Option Types addable-option

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -586,4 +586,15 @@ $options = array(
             ),
         ),
     ),
+    'option_addable_option' => array(
+        'type'  => 'addable-option',
+        'value' => array('Value 1', 'Value 2', 'Value 3'),
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        'option' => array( 'type' => 'text' ),
+        'add-button-text' => __('Add', '{domain}'),
+        'sortable' => true,
+    ),
 );
