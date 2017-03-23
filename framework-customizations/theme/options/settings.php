3f32818d75331a5a476de8c9cd69d81f9442a3de
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 12:51:56 +0200

    Options Built-in Option Types checkboxes

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -49,15 +49,39 @@ $options = array(
         ),
         'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
     ),
-    'option_single_checkbox.' => array(
+    'option_single_checkbox' => array(
         'type'  => 'checkbox',
         'value' => true,
         'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
         'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'text'  => __('Yes', MY_THEME_TEXTDOMAIN),
         'attr'  => array(
             'class' => 'custom-class',
             'data-foo' => 'bar'
         ),
         'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
-    )
+    ),
+
+    'option_single_checkboxes' => array(
+        'type'  => 'checkboxes',
+        'value' => array(
+            'choice-1' => false,
+            'choice-2' => true,
+        ),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'label' => __('Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Description', MY_THEME_TEXTDOMAIN),
+        'help'  => __('Help tip', MY_THEME_TEXTDOMAIN),
+        'choices' => array( // Note: Avoid bool or int keys http://bit.ly/1cQgVzk
+            'choice-1' => __('Choice 1', MY_THEME_TEXTDOMAIN),
+            'choice-2' => __('Choice 2', MY_THEME_TEXTDOMAIN),
+            'choice-3' => __('Choice 3', MY_THEME_TEXTDOMAIN),
+        ),
+        // Display choices inline instead of list
+        'inline' => false,
+        //'inline' => true,
+    ),
 );
