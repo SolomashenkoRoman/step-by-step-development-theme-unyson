f6206ba42cef0f9eaef98e56a21716cdc246d395
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 14:21:55 +0200

    Options Built-in Option Types datetime-range

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -348,4 +348,31 @@ $options = array(
             'defaultTime'   => '12:00' // Если входное значение пустое, timepicker установит время использования defaultTime.
         ),
     ),
+    'option_datetime_range' => array(
+        'type'  => 'datetime-range',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        'datetime-pickers' => array(
+            'from' => array(
+                'minDate' => '1970/01/01', // By default minimum date will be current day, set a date in the datetime format.
+                'maxDate' => '2038/01/19', // By default there is not maximum date , set a date in the datetime format.
+                'format'  => 'Y/m/d H:i',  // Format datetime.
+                'timepicker'  => true,     // Show timepicker.
+                'datepicker'  => true,     // Show datepicker.
+            ),
+            'to' => array(
+                'minDate' => '1970/01/01', // By default minimum date will be current day, set a date in the datetime format.
+                'maxDate' => '2038/01/19', // By default there is not maximum date , set a date in the datetime format.
+                'format'  => 'Y/m/d H:i',  // Format datetime.
+                'timepicker'  => true,     // Show timepicker.
+                'datepicker'  => true,     // Show datepicker.
+            )
+        ),
+        'value' => array(
+            'from' => '',
+            'to' => ''
+        )
+    ),
 );
