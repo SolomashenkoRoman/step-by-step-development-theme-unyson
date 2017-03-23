c35162f3a9d8976f7152bd4b1ce64abeaca2f463
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 15:51:37 +0200

    Options Built-in Option Types typography

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -858,4 +858,22 @@ $options = array(
             'keep_ratio' => true
         )
     ),
+    'option_typography' => array(
+        'type'  => 'typography',
+        'value' => array(
+            'family' => 'Arial',
+            'size'   => 12,
+            'style'  => '400',
+            'color'  => '#000000'
+        ),
+        'components' => array(
+            'family' => true,
+            'size'   => true,
+            'color'  => true
+        ),
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+    )
 );
