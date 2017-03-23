530e5a6333586dee38edbaacfd5223be049bd32d
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 15:11:29 +0200

    Options Built-in Option Types popup

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -511,4 +511,39 @@ $options = array(
         'desc'  => __('Description', '{domain}'),
         'help'  => __('Help tip', '{domain}'),
     ),
+    'option_popup' => array(
+        'type' => 'popup',
+        'value' => array(
+            'option_1' => 'value 1',
+            'option_2' => 'value 2',
+        ),
+        'label' => __('Popup', '{domain}'),
+        'desc'  => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+        'popup-title' => __('Popup Title', '{domain}'),
+        'button' => __('Edit', '{domain}'),
+        'popup-title' => null,
+        'size' => 'small', // small, medium, large
+        'popup-options' => array(
+            'option_popup_1' => array(
+                'label' => __('Text', '{domain}'),
+                'type' => 'text',
+                'value' => 'Demo text value',
+                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
+                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '{domain}')
+                ),
+            ),
+            'option_popup_2' => array(
+                'label' => __('Textarea', '{domain}'),
+                'type' => 'textarea',
+                'value' => 'Demo textarea value',
+                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
+                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '{domain}'),
+                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '{domain}')
+                ),
+            ),
+        ),
+    )
 );
