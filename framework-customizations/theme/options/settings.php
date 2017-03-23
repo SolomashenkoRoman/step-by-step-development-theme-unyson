db8d8be6cbf2f514c9b86ecba5ef312bf43d2978
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 13:33:15 +0200

    Options Built-in Option Types rgba color picker

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -232,5 +232,15 @@ $options = array(
         'label' => __('Label', '{domain}'),
         'desc'  => __('Description', '{domain}'),
         'help'  => __('Help tip', '{domain}'),
-    )
+    ),
+    'option_rgba_color_picker' => array(
+        'type'  => 'rgba-color-picker',
+        'value' => 'rgba(255,0,0,0.5)',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        // Палитра цветов
+        'palettes' => array( '#ba4e4e', '#0ce9ed', '#941940' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+    ),
 );
