d1f940c3ecfbd9d89b29c4faff6ba3d6b13d7fe0
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-23 12:49:18 +0200

    Options Built-in Option Types checkbox

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -48,5 +48,16 @@ $options = array(
             'data-foo' => 'bar'
         ),
         'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
+    ),
+    'option_single_checkbox.' => array(
+        'type'  => 'checkbox',
+        'value' => true,
+        'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
     )
 );
