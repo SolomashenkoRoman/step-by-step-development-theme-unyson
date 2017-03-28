c12c10987e94c45e7952ad83c337603c9ca93b60
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-28 11:24:00 +0300

    Built-in Extensions: Shortcodes: Создание нового шорткода: Cookbook: config file

---
+++ framework-customizations/extensions/shortcodes/shortcodes/hr/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.03.17
+ * Time: 11:21 AM
+ */
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+$cfg = array(
+    'page_builder' => array(
+        'title'       => __('Horizontal Ruler', '{domain}'),
+        'description' => __('Creates a \'hr\' html tag', '{domain}'),
+        'tab'         => __('Demo Elements', '{domain}'),
+        //'tab'         => __('My Elements', '{domain}'),
+    )
+);
---
+++ framework-customizations/extensions/shortcodes/shortcodes/my-shortcode/class-fw-shortcode-my-shortcode.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.03.17
+ * Time: 11:02 AM
+ */
+class FW_Shortcode_My_Shortcode extends FW_Shortcode
+{
+
+}
