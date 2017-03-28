2f3cdc3118187eb000e497aabd119d270a126679
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-28 23:18:34 +0300

    Built-in Extensions: Slider: demo-slider manifest

---
+++ framework-customizations/extensions/media/extensions/slider/demo-slider/manifest.php
@@ -0,0 +1,12 @@
+<?php
+if ( ! defined( 'FW' ) ) {
+    die( 'Forbidden' );
+}
+
+$manifest = array();
+
+/*$manifest['name']        = __( 'My extension demo slider', MY_THEME_TEXTDOMAIN );
+$manifest['description'] = __( 'My extension demo slider description', MY_THEME_TEXTDOMAIN );
+$manifest['version'] = '0.0.1';
+$manifest['display'] = true;*/
+$manifest['standalone'] = true;
