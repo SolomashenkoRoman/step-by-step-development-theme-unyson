f6eb630e8dcaf1ba6b8ef9faf4029ba3f8979102
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-28 12:27:14 +0300

    Built-in Extensions: Shortcodes: Создание нового шорткода: Cookbook: my-table-builder shortcode configfile

---
+++ framework-customizations/extensions/shortcodes/shortcodes/my-table-builder/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.03.17
+ * Time: 12:25 PM
+ */
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+$cfg = array(
+    'page_builder' => array(
+        'title'       => __('My Table Builder', '{domain}'),
+        'description' => __('Creates custom tables', '{domain}'),
+        'tab'         => __('Demo Elements', '{domain}'),
+        'popup_size'  => 'large'
+    )
+);
