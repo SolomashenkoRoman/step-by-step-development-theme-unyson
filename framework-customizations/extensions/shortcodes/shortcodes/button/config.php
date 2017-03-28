6ff5f4327ca47f645c380576283703e2a6098c77
Roman Solomashenko <romansolomashenko@Romans-Mac-mini.local>
2017-03-28 11:36:48 +0300

    Built-in Extensions: Shortcodes: Создание нового шорткода: Cookbook: button shortcode config file

---
+++ framework-customizations/extensions/shortcodes/shortcodes/button/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.03.17
+ * Time: 11:35 AM
+ */
+if (!defined('FW')) {
+    die('Forbidden');
+}
+
+$cfg = array(
+    'page_builder' => array(
+        'title'         => __('Button', '{domain}'),
+        'description'   => __('Creates a button with choosable label, size and style', '{domain}'),
+        'tab'           => __('Demo Elements', '{domain}'),
+    )
+);
---
+++ framework-customizations/extensions/shortcodes/shortcodes/hr/views/view.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romansolomashenko
+ * Date: 28.03.17
+ * Time: 11:29 AM
+ */
+if (!defined('FW')){
+    die('Forbidden');
+}
+?>
+
+<hr>
